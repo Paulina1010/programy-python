90d142ea2eb4401b757627be58969cb7534f1851
Paulina1010 <paulina.kucinska10@gmail.com>
2024-08-13 20:14:20 +0200

    Define namedtuple using next
    Next takes the first line from the csv file and moves on to the next line.

--- readcsv.py
+++ readcsv.py
@@ -3,7 +3,7 @@ import csv
 
 with open('products.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-    products = namedtuple('products', 'Product, Value, Price')
+    products = namedtuple('products', next(spamreader))
 
     for emp in map(products._make, spamreader):
         print(emp)

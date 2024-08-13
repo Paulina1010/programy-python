e68a0f711d7d806e90fca06a222fb3821c93f419
Paulina1010 <paulina.kucinska10@gmail.com>
2024-08-13 20:14:21 +0200

    Load data into a SQLITE temporary database

--- readcsv.py
+++ readcsv.py
@@ -1,9 +1,17 @@
 from collections import namedtuple
 import csv
+import sqlite3
+
+con = sqlite3.connect(":memory:") #połączenie się do bazy tymczasowej, w RAM
+#cur = con.cursor() #utworzenie kursora
 
 with open('products.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-    products = namedtuple('products', next(spamreader))
-
+    n = next(spamreader)
+    products = namedtuple('products', n)
+    print(n)
+    con.execute("CREATE TABLE products(%s, %s, %s)" % tuple(n)) #tak nie robić!!!
     for emp in map(products._make, spamreader):
-        print(emp)
+        con.execute("INSERT INTO products VALUES(?, ?, ?)", emp)
+    for row in con.execute("SELECT * FROM products"):
+        print(row)

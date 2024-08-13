6459bd0a7bbafbefb7c44e9a3c78fe82ac7966ef
Paulina1010 <paulina.kucinska10@gmail.com>
2024-08-13 20:14:24 +0200

    Add error handling and clean up the program

--- readcsv.py
+++ readcsv.py
@@ -1,26 +1,54 @@
+#Program dodatkowo wykonuje tylko jedną operację, która jest podana przy wywoływaniu programu np. python readcsv7.py display
 from collections import namedtuple
 import csv
 import sqlite3
 import sys
 
 con = sqlite3.connect("database") #połączenie się do bazy database, która zostanie automatycznie utworzona po wywołaniu tego polecenia
-print(sys.argv)
 
-if (sys.argv[1]=="drop"):
+if sys.argv[1] == "drop":
     con.execute("DROP TABLE products")
+    print("Usunięto tabelę", file=sys.stderr)
     
-else:
+elif sys.argv[1] == "create":
     with open('products.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-        n = next(spamreader)
-        products = namedtuple('products', n)
-        print(n)
-        if (sys.argv[1]=="create"):
-            con.execute("CREATE TABLE products(%s, %s, %s)" % tuple(n)) #tak nie robić!!!
-        if (sys.argv[1]=="load"):
-            for emp in map(products._make, spamreader):
-                con.execute("INSERT INTO products VALUES(?, ?, ?)", emp)
-                con.commit()
-        if(sys.argv[1]=="display"):
-            for row in con.execute("SELECT * FROM products"):
-                print(row)
+        header = next(spamreader)
+        con.execute("CREATE TABLE products(%s, %s, %s)" % tuple(header)) #tak nie robić!!!
+    print("Utworzono tabelę", file=sys.stderr)
+    
+elif sys.argv[1] == "load":
+    with open('products.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+        header = next(spamreader)
+        products = namedtuple('products', header)
+        n = 0
+        for emp in map(products._make, spamreader):
+            con.execute("INSERT INTO products VALUES(?, ?, ?)", emp)
+            con.commit() #jest potrzebny commit, ponieważ podczas wywoływania INSERTa w SQLITE automatycznie jest rozpoczynana transakcja, więc musimy ją skomitować
+            n += 1 #licznik dodanych rekordów
+    print("Załadowano %d rekordy" % n, file=sys.stderr)
+    
+elif sys.argv[1] == "display":
+    try:
+        cur = con.execute("SELECT * FROM products")
+        empty = True
+        for row in cur:
+            print(*row) #print(row[0], row[1], row[2], ...) * rozwija krotkę albo listę na argumenty
+            empty = False
+        if empty:
+            print("Nie ma danych w tabeli", file=sys.stderr)
+    except sqlite3.OperationalError:
+        print("Upps! Tabela nie istnieje", file=sys.stderr)
+        sys.exit(1)
+
+      
+else:
+    print("""
+Podałeś nieznaną operację. Możesz:
+ 1) usunąć tabelę - drop
+ 2) utworzyć tabelę - create
+ 3) załadować dane do tabeli - load
+ 4) wyświetlić dane z tabeli - display
+            """, file=sys.stderr)
+    sys.exit(1)

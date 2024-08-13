b68479634b629cea23589fd0e26a495e4c01c885
Paulina1010 <paulina.kucinska10@gmail.com>
2024-08-13 20:14:23 +0200

    Add DROP TABLE command and add argv
    The user can choose the operation they want to perform.

--- readcsv.py
+++ readcsv.py
@@ -1,17 +1,26 @@
 from collections import namedtuple
 import csv
 import sqlite3
+import sys
 
-con = sqlite3.connect(":memory:") #połączenie się do bazy tymczasowej, w RAM
-#cur = con.cursor() #utworzenie kursora
+con = sqlite3.connect("database") #połączenie się do bazy database, która zostanie automatycznie utworzona po wywołaniu tego polecenia
+print(sys.argv)
 
-with open('products.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-    n = next(spamreader)
-    products = namedtuple('products', n)
-    print(n)
-    con.execute("CREATE TABLE products(%s, %s, %s)" % tuple(n)) #tak nie robić!!!
-    for emp in map(products._make, spamreader):
-        con.execute("INSERT INTO products VALUES(?, ?, ?)", emp)
-    for row in con.execute("SELECT * FROM products"):
-        print(row)
+if (sys.argv[1]=="drop"):
+    con.execute("DROP TABLE products")
+    
+else:
+    with open('products.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+        n = next(spamreader)
+        products = namedtuple('products', n)
+        print(n)
+        if (sys.argv[1]=="create"):
+            con.execute("CREATE TABLE products(%s, %s, %s)" % tuple(n)) #tak nie robić!!!
+        if (sys.argv[1]=="load"):
+            for emp in map(products._make, spamreader):
+                con.execute("INSERT INTO products VALUES(?, ?, ?)", emp)
+                con.commit()
+        if(sys.argv[1]=="display"):
+            for row in con.execute("SELECT * FROM products"):
+                print(row)
